434de017014d8508af79cd0c9ddeb6f9aacefc55
ryo <ryokokeguchi@gmail.com>
2021-04-10 14:15:57 +0900

    create app

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCoXGM-X0_6bofjOJR6MNoqmDX5NXlidVM",
+  authDomain: "idobatakaigi-hands-on.firebaseapp.com",
+  projectId: "idobatakaigi-hands-on",
+  storageBucket: "idobatakaigi-hands-on.appspot.com",
+  messagingSenderId: "974466325558",
+  appId: "1:974466325558:web:6bcf82203f1c8bc75d8c97"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const messageRef = database.ref('messages');
+
+export const pushMessage = ({name, text}) => {
+  messageRef.push({ name, text })
+}
